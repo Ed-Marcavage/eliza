d51448732a148db9500a2cddf743a2fa2c3f0bcf
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2024-12-28 16:47:17 -0500

    docs: Add JSDoc documentation to types.ts

--- packages/core/src/test_resources/types.ts
+++ packages/core/src/test_resources/types.ts
@@ -1,3 +1,11 @@
+/**
+ * Represents a User object with the following properties.
+ * @typedef {object} User
+ * @property {string} id - The unique identifier for the user.
+ * @property {string} [email] - The email address of the user (optional).
+ * @property {string} [phone] - The phone number of the user (optional).
+ * @property {string} [role] - The role of the user (optional).
+ */
 export interface User {
     id: string;
     email?: string;
