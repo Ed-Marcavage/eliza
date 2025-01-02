a6a0dce4b5a2848a2e57280002f49913971afb9b
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2025-01-02 01:08:56 -0500

    docs: Add JSDoc documentation to token.ts

--- packages/plugin-starknet/src/types/token.ts
+++ packages/plugin-starknet/src/types/token.ts
@@ -1,3 +1,16 @@
+/**
+ * Interface for defining a quote request.
+ * @property {string} sellTokenAddress - The address of the token being sold.
+ * @property {string} buyTokenAddress - The address of the token being bought.
+ * @property {bigint} [sellAmount] - The amount of tokens being sold.
+ * @property {bigint} [buyAmount] - The amount of tokens being bought.
+ * @property {string} [takerAddress] - The address which will fill the quote.
+ * @property {number} [size] - The maximum number of returned quotes.
+ * @property {string[]} [excludeSources] - The sources that the user wants to exclude.
+ * @property {bigint} [integratorFees] - Fee amount in bps, where 30 is 0.3%.
+ * @property {string} [integratorFeeRecipient] - Required when integratorFees is defined, the address of the fee collector.
+ * @property {string} [integratorName] - The name of the application making the request.
+ */
 interface QuoteRequest {
     sellTokenAddress: string;
     buyTokenAddress: string;
@@ -17,6 +30,33 @@ interface QuoteRequest {
     integratorName?: string; // AVNU Portal
 }
 
+/**
+ * Interface representing a Quote object containing information about a quote for a token swap.
+ * @typedef { Object } Quote
+ * @property { string } quoteId - The unique id of the quote.
+ * @property { string } sellTokenAddress - The address of the token being sold.
+ * @property { bigint } sellAmount - The amount of token being sold.
+ * @property { number } sellAmountInUsd - The amount of token being sold in USD.
+ * @property { string } buyTokenAddress - The address of the token being bought.
+ * @property { bigint } buyAmount - The amount of token being bought.
+ * @property { number } buyAmountInUsd - The amount of token being bought in USD.
+ * @property { number } [blockNumber] - The block number related to the quote.
+ * @property { string } chainId - The chain id of the quote.
+ * @property { number } [expiry] - The Unix timestamp when the quote expires in seconds.
+ * @property {Route[]} routes - An array of Route objects representing the route for the quote.
+ * @property { bigint } gasFees - The estimated amount of gas fees in ETH.
+ * @property { number } gasFeesInUsd - The estimated amount of gas fees in USD.
+ * @property { bigint } avnuFees - The actual fees taken by AVNU.
+ * @property { number } avnuFeesInUsd - The actual fees taken by AVNU in USD.
+ * @property { bigint } avnuFeesBps - The fees in bps taken by AVNU.
+ * @property { bigint } integratorFees - The actual fees taken by the integrator.
+ * @property { number } integratorFeesInUsd - The actual fees taken by the integrator in USD.
+ * @property { bigint } integratorFeesBps - The fees in bps taken by the integrator.
+ * @property { number } priceRatioUsd - The price ratio in USD and in bps.
+ * @property { number } [sellTokenPriceInUsd] - The sell token price in USD.
+ * @property { number } [buyTokenPriceInUsd] - The buy token price in USD.
+ * @property { Gasless } gasless - An object containing information about gasless transactions.
+ */
 interface Quote {
     // The unique id of the quote
     quoteId: string;
@@ -56,6 +96,16 @@ interface Quote {
     gasless: Gasless;
 }
 
+/**
+ * Interface for defining a route containing information about the source, address, percentage distribution, and token addresses.
+ * @typedef {Object} Route
+ * @property {string} name - The name of the source (e.g. 10kSwap)
+ * @property {string} address - The address of the source
+ * @property {number} percent - The percentage distribution of the sellToken (1 representing 100%)
+ * @property {string} sellTokenAddress - The address of the sell token
+ * @property {string} buyTokenAddress - The address of the buy token
+ * @property {Route[]} routes - An array of nested routes
+ */
 interface Route {
     // The name of the source
     name: string; // 10kSwap
@@ -68,6 +118,14 @@ interface Route {
     routes: Route[];
 }
 
+/**
+ * Interface representing gasless transaction information.
+ * @property {boolean} active - Indicates if gasless transactions are active.
+ * @property {Object[]} gasTokenPrices - Array of gas token prices.
+ * @property {string} gasTokenPrices.tokenAddress - The address of the gas token.
+ * @property {number} gasTokenPrices.gasFeesInUsd - The gas fees in USD.
+ * @property {bigint} gasTokenPrices.gasFeesInGasToken - The gas fees in gas token.
+ */
 export interface Gasless {
     active: boolean;
     gasTokenPrices: {
@@ -77,6 +135,32 @@ export interface Gasless {
     }[];
 }
 
+/**
+ * Interface representing information about a token.
+ * @typedef {Object} TokenInfo
+ * @property {string} name - The name of the token.
+ * @property {string} symbol - The symbol of the token.
+ * @property {string} address - The address of the token.
+ * @property {string} logoUri - The URI of the token's logo.
+ * @property {string} coingeckoId - The Coingecko ID of the token.
+ * @property {boolean} verified - Indicates if the token is verified.
+ * @property {Object} market - Information about the market of the token.
+ * @property {number} currentPrice - The current price of the token.
+ * @property {number} marketCap - The market capitalization of the token.
+ * @property {number} fullyDilutedValuation - The fully diluted valuation of the token.
+ * @property {number} starknetTvl - The Starknet Total Value Locked of the token.
+ * @property {number} priceChange1h - The price change in the last 1 hour.
+ * @property {number} priceChangePercentage1h - The price change percentage in the last 1 hour.
+ * @property {number} priceChange24h - The price change in the last 24 hours.
+ * @property {number} priceChangePercentage24h - The price change percentage in the last 24 hours.
+ * @property {number} priceChange7d - The price change in the last 7 days.
+ * @property {number} priceChangePercentage7d - The price change percentage in the last 7 days.
+ * @property {number} marketCapChange24h - The market capitalization change in the last 24 hours.
+ * @property {number} marketCapChangePercentage24h - The market capitalization change percentage in the last 24 hours.
+ * @property {number} starknetVolume24h - The Starknet volume in the last 24 hours.
+ * @property {number} starknetTradingVolume24h - The Starknet trading volume in the last 24 hours.
+ * @property {string[]} tags - Array of tags related to the token.
+ */
 export interface TokenInfo {
     name: string;
     symbol: string;
