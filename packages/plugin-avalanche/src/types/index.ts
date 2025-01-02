402cbe7bf7a6edcdbe2f9dec430d7c40a1d0032b
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2025-01-01 23:59:06 -0500

    docs: Add JSDoc documentation to index.ts

--- packages/plugin-avalanche/src/types/index.ts
+++ packages/plugin-avalanche/src/types/index.ts
@@ -1,5 +1,13 @@
 import { Address } from "viem";
 
+/**
+ * Interface representing a YakSwapQuote object.
+ * @typedef {Object} YakSwapQuote
+ * @property {bigint[]} amounts - Array of amounts for the swap.
+ * @property {Address[]} adapters - Array of adapter addresses used for the swap.
+ * @property {Address[]} path - Array of addresses representing the swap path.
+ * @property {bigint} gasEstimate - Gas estimate for the swap.
+ */
 interface YakSwapQuote {
     amounts: bigint[];
     adapters: Address[];
@@ -19,6 +27,20 @@ interface YakSwapQuote {
 //     uint256[] askPrices;
 //     bytes args;
 // }
+/**
+ * Interface representing the parameters required for creating a TokenMill market.
+ * @typedef {object} TokenMillMarketCreationParameters
+ * @property {number} tokenType - The type of the token.
+ * @property {string} name - The name of the token.
+ * @property {string} symbol - The symbol of the token.
+ * @property {Address} quoteToken - The address of the quote token.
+ * @property {bigint} totalSupply - The total supply of the token.
+ * @property {number} creatorShare - The share for the creator.
+ * @property {number} stakingShare - The share for staking.
+ * @property {bigint[]} bidPrices - The bid prices for the market.
+ * @property {bigint[]} askPrices - The ask prices for the market.
+ * @property {string} args - Additional arguments for the market creation.
+ */
 interface TokenMillMarketCreationParameters {
     tokenType: number;
     name: string;
