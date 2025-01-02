06a5dcee7fa2510353dbf168ae58212493942cb0
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2025-01-02 00:03:58 -0500

    docs: Add JSDoc documentation to unruggable.ts

--- packages/plugin-starknet/src/actions/unruggable.ts
+++ packages/plugin-starknet/src/actions/unruggable.ts
@@ -25,6 +25,14 @@ import { validateStarknetConfig } from "../environment.ts";
 //     sellAmount: string;
 // }
 
+/**
+ * Interface representing the content for deploying a token.
+ * @interface
+ * @property {string} name - The name of the token.
+ * @property {string} symbol - The symbol of the token.
+ * @property {string} owner - The owner of the token.
+ * @property {string} initialSupply - The initial supply of the token.
+ */
 interface DeployTokenContent {
     name: string;
     symbol: string;
@@ -32,6 +40,11 @@ interface DeployTokenContent {
     initialSupply: string;
 }
 
+/**
+ * Check if the given object is a valid DeployTokenContent by validating its properties.
+ * @param {DeployTokenContent} content - The content to be validated.
+ * @returns {boolean} - true if the content is a valid DeployTokenContent, false otherwise.
+ */
 export function isDeployTokenContent(content: DeployTokenContent) {
     // Validate types
     const validTypes =
